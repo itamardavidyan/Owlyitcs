f3907e8caed4dfceb2c2919023aa067c9c1377a0
Itamar Davidyan <itamrd@beeriprint.co.il>
2021-08-09 23:19:05 +0300

    Create new tests

--- cypress/integration/sample.spec.js
+++ cypress/integration/sample.spec.js
@@ -41,4 +41,61 @@ describe('My First Test', () => {
         cy.get('#result').find('li:nth-child(2)').contains('Elior')
         cy.get('#result').find('li:nth-child(3)').contains('Yaron')
     })
+
+    it('Case 6', () => {
+        cy.get('textarea').type(`
+           Yaron    Owlyitcs
+               Owlyitcs   Owlyitcs
+               
+               Elior Elior
+                Elior   
+                `)
+        cy.get('#submitForm').click()
+
+        cy.get('#result').find('li').should('have.length', 3)
+        cy.get('#result').find('li:nth-child(1)').contains('Owlyitcs')
+        cy.get('#result').find('li:nth-child(2)').contains('Elior')
+        cy.get('#result').find('li:nth-child(3)').contains('Yaron')
+    })
+
+    it('Case 7', () => {
+        cy.get('textarea').type('aa bb bb cc cc cc dd dd dd dd ee ee ee ee ee')
+        cy.get('#submitForm').click()
+
+        cy.get('#result').find('li').should('have.length', 5)
+        cy.get('#result').find('li:nth-child(1)').contains('ee')
+        cy.get('#result').find('li:nth-child(2)').contains('dd')
+        cy.get('#result').find('li:nth-child(3)').contains('cc')
+        cy.get('#result').find('li:nth-child(4)').contains('bb')
+        cy.get('#result').find('li:nth-child(5)').contains('aa')
+    })
+
+
+    it('Case 8', () => {
+        cy.get('textarea').type('ff aa aa bb bb bb cc cc cc cc dd dd dd dd dd ee ee ee ee ee ee')
+        cy.get('#submitForm').click()
+
+        cy.get('#result').find('li').should('have.length', 5)
+        cy.get('#result').find('li:nth-child(1)').contains('ee')
+        cy.get('#result').find('li:nth-child(2)').contains('dd')
+        cy.get('#result').find('li:nth-child(3)').contains('cc')
+        cy.get('#result').find('li:nth-child(4)').contains('bb')
+        cy.get('#result').find('li:nth-child(5)').contains('aa')
+    })
+
+    it('Case 9', () => {
+        cy.get('textarea').type('nnn nnn \t \t \t  \\n \\n \n \\n \\n nnn nnn \t \t \t nnn')
+        cy.get('#submitForm').click()
+
+        cy.get('#result').find('li').should('have.length', 2)
+        cy.get('#result').find('li:nth-child(1)').contains('nnn')
+        cy.get('#result').find('li:nth-child(2)').contains('\\n')
+    })
+
+    it('Case 10', () => {
+        cy.get('textarea').type('abcdef abcde abcd abc ab c ab abc abcd abcde abcdef')
+        cy.get('#submitForm').click()
+
+        cy.get('#result').find('li').should('have.length', 5)
+    })
 })
